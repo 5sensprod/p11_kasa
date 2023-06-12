e535166d26de0f4a9e14b4095061cb8ab4187b58
chris <5sensprod@gmail.com>
2023-06-13 01:34:35 +0200

    Stylisation dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from 'react'
+import styles from './Dropdown.module.css'
+import chevronDown from '../../assets/chevron-down.svg'
+import chevronUp from '../../assets/chevron-up.svg'
 
 function Dropdown({ title, description }) {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div onClick={() => setIsOpen(!isOpen)}>
-      <h2>{title}</h2>
-      {isOpen && <p>{description}</p>}
+    <div className={styles.dropdown}>
+      <button
+        className={styles.dropdownButton}
+        onClick={() => setIsOpen(!isOpen)}
+        aria-expanded={isOpen}
+      >
+        <span className={styles.title}>{title}</span>
+        <img
+          src={isOpen ? chevronUp : chevronDown}
+          alt="chevron"
+          className={styles.chevron}
+        />
+      </button>
+      {isOpen && <div className={styles.description}>{description}</div>}
     </div>
   )
 }
