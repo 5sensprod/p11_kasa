581f4dfff7f438698b0c85ce8a8f106e085288a1
chris <5sensprod@gmail.com>
2023-06-13 08:22:33 +0200

    Ajout SLideshow component + useNavigate()

---
+++ src/components/Slideshow/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import styles from './Slideshow.module.css'
+
+function Slideshow({ pictures }) {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev === pictures.length - 1 ? 0 : prev + 1))
+  }
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev === 0 ? pictures.length - 1 : prev - 1))
+  }
+
+  return (
+    <div className={styles.slideshow}>
+      {pictures.length > 1 && (
+        <>
+          <button className={styles.prev} onClick={prevSlide}>
+            &#10094;
+          </button>
+          <button className={styles.next} onClick={nextSlide}>
+            &#10095;
+          </button>
+        </>
+      )}
+      {pictures.map((imgUrl, index) => (
+        <div
+          className={`${styles.slide} ${
+            index === currentSlide ? styles.active : ''
+          }`}
+          key={index}
+        >
+          <img src={imgUrl} alt={`Slide ${index}`} />
+        </div>
+      ))}
+      {pictures.length > 1 && (
+        <div className={styles.slideNumber}>
+          {currentSlide + 1} / {pictures.length}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Slideshow
--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,35 +1,37 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import styles from './Housing.module.css'
 import Dropdown from '../../components/Dropdown'
 import Tags from '../../components/Tags'
 import StarRating from '../../components/StarRating'
 import HostInfo from '../../components/HostInfo'
+import Slideshow from '../../components/Slideshow'
 
 function Housing() {
   const { id } = useParams()
   const [property, setProperty] = useState(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetch(`/data/logements.json`)
       .then((response) => response.json())
       .then((data) => {
         const selectedProperty = data.find((property) => property.id === id)
-        setProperty(selectedProperty)
+        if (!selectedProperty) {
+          navigate('/not-found')
+        } else {
+          setProperty(selectedProperty)
+        }
       })
-  }, [id])
+  }, [id, navigate])
 
   if (!property) {
-    return <div>Loading...</div>
+    return null
   }
 
   return (
     <main className={styles.housingContainer}>
-      <img
-        src={property.cover}
-        alt={property.title}
-        className={styles.imgHousing}
-      />
+      <Slideshow pictures={property.pictures} />
       <h1 className={styles.title}>{property.title}</h1>
       <p className={styles.location}>{property.location}</p>
       <Tags tags={property.tags} />
