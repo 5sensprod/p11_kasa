e58a036d501f4468de0bd4686e24ae7b726b70e2
chris <5sensprod@gmail.com>
2023-06-16 14:32:09 +0200

    madif basename

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [marginBottomSize, setMarginBottomSize] = useState('small')
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <MarginBottomContext.Provider
         value={{ marginBottomSize, setMarginBottomSize }}
       >
