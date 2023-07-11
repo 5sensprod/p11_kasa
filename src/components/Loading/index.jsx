96ae971a149115b561202b1bb213b0f2b102dbcc
chris <5sensprod@gmail.com>
2023-07-11 15:25:46 +0200

    indentation css + correction comm.

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-// Le composant Loading est utilisé pour indiquer à l'utilisateur que quelque chose est en cours de chargement.
-// Il peut être affiché lorsqu'une opération asynchrone est en cours, par exemple lors de l'appel à une API.
+// Le composant Loading est utilisé dans la page Home.
+// Il peut être affiché lorsqu'une opération asynchrone est en cours, lors de l'appel à l'API.
 
 function Loading() {
   return <h2>Loading...</h2>
